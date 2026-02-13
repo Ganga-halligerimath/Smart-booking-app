a6671932b45b84997ae71681c01f1723c1e72d3a
Ganga <gangadevihalligerimath@gmail.com>
2026-02-13 21:17:32 +0530

    Update dashboard & bookmarklet code with TS fixes

--- src/pages/api/bookmarklet.ts
+++ src/pages/api/bookmarklet.ts
@@ -8,6 +8,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   res.setHeader("Access-Control-Allow-Origin",allowedOrigin)
   res.setHeader("Access-Control-Allow-Methods", "OPTIONS, POST")
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization")
+   res.setHeader("Access-Control-Allow-Credentials", "true"); 
 
   if (req.method === "OPTIONS") return res.status(200).end()
   if (req.method !== "POST") return res.status(405).end()
